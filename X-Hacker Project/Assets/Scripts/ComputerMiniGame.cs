6425ac0e702946260d33f02ee6eeece871139960
Brandon <brandonjorgen@gmail.com>
2020-10-10 14:45:05 -0600

    Computer minigame progress

---
+++ X-Hacker Project/Assets/Scripts/ComputerMiniGame.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "Mini-Games/Computer")]
+public class ComputerMiniGame : MiniGameBase
+{
+    private int buttonPresses;
+    public UnityEvent successEvent;
+    public override void StartMiniGame()
+    {
+        Debug.Log("Computer mini-game started");
+    }
+
+    public override void Success()
+    {
+        if (buttonPresses > 0)
+        {
+            successEvent.Invoke();
+        }
+    }
+
+    public override void Difficulty()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ResetMiniGame()
+    {
+        buttonPresses = 0;
+    }
+}
