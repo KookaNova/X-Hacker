c479c8fc07cbf459359a210783df9026051945d3
Brandon <brandonjorgen@gmail.com>
2020-10-10 10:34:18 -0600

    Created Base Script

---
+++ X-Hacker Project/Assets/Scripts/MiniGameBase.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class MiniGameBase : ScriptableObject
+{
+    /*
+        All Mini-Games must inherit from this script
+        These functions must be used in every Mini-Game
+        
+        -Brandon
+    */
+    public virtual void StartMiniGame()
+    {
+        
+    }
+
+    public virtual void CloseMiniGame()
+    {
+        
+    }
+
+    public virtual void Objective()
+    {
+        
+    }
+
+    public virtual void Success()
+    {
+        
+    }
+
+    public virtual void Difficulty()
+    {
+        
+    }
+}
