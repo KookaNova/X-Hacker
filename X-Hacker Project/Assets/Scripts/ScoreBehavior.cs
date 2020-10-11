517fd7111d2d44625d7fa34c1766aca3e25f0a54
Connor (Home) <connorcoxr@gmail.com>
2020-10-11 12:26:34 -0600

    ScoreBehavior script added

---
+++ X-Hacker Project/Assets/Scripts/ScoreBehavior.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBehavior : MonoBehaviour
+{
+    public FloatData score;
+    public Text scoreText;
+
+    private void Update()
+    {
+        scoreText.text = "" + score.value;
+    }
+}
