b0f2f4ef2fcd99ce330b651a2fcdf84cb6819b00
Connor (Home) <connorcoxr@gmail.com>
2020-10-11 12:18:09 -0600

    Improved FloatData script

--- X-Hacker Project/Assets/Scripts/FloatDataSO.cs
+++ X-Hacker Project/Assets/Scripts/FloatDataSO.cs
@@ -1,8 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FloatDataSO : ScriptableObject
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
 {
     public float value;
+   
+    public void SetValue (float amount)
+    {
+        value = amount;
+    }
+   
+    public void UpdateValue(float amount)
+    {
+        value += amount;
+    }
+   
+    public void IncrementValue()
+    {
+        value ++;
+    }
+   
+    public void UpdateValue(FloatData data)
+    {
+        var newData = data as FloatData;
+        if (newData != null) value += newData.value;
+    }
+
+    public void SetValue(FloatData data)
+    {
+        var newData = data as FloatData;
+        if (newData != null) value = newData.value;
+    }
+    
+    public void CheckMinValue(float minValue)
+    {
+        if (value <= minValue)
+        {
+            value = minValue;
+        }
+    }
+
+    public void CheckMaxValue(float maxValue)
+    {
+        if (value >= maxValue)
+        {
+            value = maxValue;
+        }
+    }
 }
