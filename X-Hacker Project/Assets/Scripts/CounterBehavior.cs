c32277e7d6003d6fb230842deb72df9221a9de13
Connor (Home) <connorcoxr@gmail.com>
2020-10-11 14:20:03 -0600

    Counter script for counting time

---
+++ X-Hacker Project/Assets/Scripts/CounterBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CounterBehavior : MonoBehaviour
+{
+
+    public UnityEvent OnCountEvent, OnEndEvent;
+
+    public int Value = 0, MaxValue = 1000;
+    //1000 seconds is approximately 16 minutes to complete the objective. More than enough.
+    public float WaitTime = 1;
+    public FloatDataSO countedSO;
+
+    public void StartCounter()
+    {
+        StartCoroutine(RunCounter());
+    }
+
+    private IEnumerator RunCounter()
+    {
+        var waitObject = new WaitForSeconds(WaitTime);
+        
+        while (Value > MaxValue)
+        {
+            yield return waitObject;
+            OnCountEvent.Invoke();
+            Value++;
+        }
+
+        yield return waitObject;
+        OnEndEvent.Invoke();
+        //Idea is that the counter gets too high and you get fired a.k.a. you lose.
+        //We would set the max really high though.
+    }
+}
