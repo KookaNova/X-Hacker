80c6affc1f6a206a3ef25ade918130246bba0db2
Brandon <brandonjorgen@gmail.com>
2020-10-17 13:09:18 -0600

    Added character controller move speed script back
    will be useful for diagonal movement testing

---
+++ X-Hacker Project/Assets/Scripts/Debug Scripts/CharControllerMovementDebug.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CharControllerMovementDebug : MonoBehaviour
+{
+    private Vector3 originalPosition;
+    private WaitForSeconds waitTime;
+    public float controllerSpeed;
+
+    private void Start()
+    {
+        waitTime = new WaitForSeconds(1f);
+        StartCoroutine(CalculateSpeed());
+    }
+
+    private IEnumerator CalculateSpeed()
+    {
+        while (true)
+        {
+            originalPosition = transform.position;
+            yield return waitTime;
+            controllerSpeed = (transform.position - originalPosition).magnitude;
+            Debug.Log(controllerSpeed);
+        }
+    }
+}
