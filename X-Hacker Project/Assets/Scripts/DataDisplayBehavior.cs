40bc5d8ca56534916c021e15ba9675825a361a04
Connor (Home) <connorcoxr@gmail.com>
2020-10-18 18:07:25 -0600

    Counter Displays Decimals, Canvas in LayoutB

--- X-Hacker Project/Assets/Scripts/DataDisplayBehavior.cs
+++ X-Hacker Project/Assets/Scripts/DataDisplayBehavior.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class DataDisplayBehavior : MonoBehaviour
@@ -8,6 +9,6 @@ public class DataDisplayBehavior : MonoBehaviour
 
     private void Update()
     {
-        dataText.text = "" + floatData.value;
+        dataText.text = floatData.value.ToString("####.##");
     }
 }
