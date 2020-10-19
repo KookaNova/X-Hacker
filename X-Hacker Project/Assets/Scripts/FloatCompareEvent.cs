63f22e382d7e06d61da28122564086c6bf5610e2
Connor (Home) <connorcoxr@gmail.com>
2020-10-18 19:38:34 -0600

    Script to compare two float datas

---
+++ X-Hacker Project/Assets/Scripts/FloatCompareEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FloatCompareEvent : MonoBehaviour
+{
+   public UnityEvent compareEvent;
+   public FloatDataSO chosenData;
+   public FloatDataSO comparedData;
+
+   private void Update()
+   {
+      if (chosenData.value >= comparedData.value)
+      {
+         compareEvent.Invoke();
+      }
+   }
+}
