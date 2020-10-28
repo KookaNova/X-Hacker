45b5f4f701a0dad5b3cddb608e6d5407dba159ec
Connor (Home) <connorcoxr@gmail.com>
2020-10-28 17:55:21 -0600

    Task system more robust, task data store additional info

--- X-Hacker Project/Assets/Scripts/TaskDataSO.cs
+++ X-Hacker Project/Assets/Scripts/TaskDataSO.cs
@@ -5,7 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Task", menuName = "Task System/New Task")]
 public class TaskDataSO : ScriptableObject
 {
-    [TextArea(15,20)] 
+    [TextArea(10,10)] 
     public string description;
+
+    public int completedSteps = 0;
+    public int totalSteps = 1;
     public bool complete = false;
+    
+    public void SetValue (int amount)
+    {
+        completedSteps = amount;
+    }
+
+    public void IncrementValue()
+    {
+        completedSteps++;
+    }
+
+    public void BoolState(bool state)
+    {
+        complete = state;
+    }
 }
--- X-Hacker Project/Assets/Scripts/TaskHolder.cs
+++ X-Hacker Project/Assets/Scripts/TaskHolder.cs
@@ -7,28 +7,50 @@ using UnityEngine.UI;
 public class TaskHolder : MonoBehaviour
 {
     public List<TaskDataSO> taskList;
-    public int totalCompleted = 0;
+    public int totalCompleted;
     public bool allTasksCompleted = false;
     private Text _taskText;
 
     public void Start()
     {
         _taskText = GetComponent<Text>();
+        foreach (var t in taskList)
+        {
+            t.completedSteps = 0;
+            t.complete = false;
+        }
+        
         CreateTaskList();
     }
 
-    private void CreateTaskList()
+    public void CreateTaskList()
     {
+        _taskText.text = null;
+        
         for (int i = 0; i < taskList.Count; i++)
         {
+            //checks if all steps are complete
+            if (taskList[i].completedSteps == taskList[i].totalSteps)
+            {
+                taskList[i].complete = true;
+            }
+            
+            //checks if task is complete
             if (taskList[i].complete == true)
             {
                 totalCompleted = totalCompleted + 1;
-                _taskText.text = _taskText.text + (i+1) + ". " + taskList[i].description + " Complete " + "\n";
+                _taskText.text = _taskText.text + (i+1) + ". " + taskList[i].description + " COMPLETE " + "\n";
             }
             else
             {
+                //checks amount of tasks needed to complete
+                if (taskList[i].totalSteps > 1)
+                {
+                    _taskText.text = _taskText.text + (i+1) + ". " + taskList[i].description + " (" + taskList[i].completedSteps + "/" + taskList[i].totalSteps + ")" + "\n";
+                }
+                else
                 _taskText.text = _taskText.text + (i+1) + ". " + taskList[i].description + "\n";
+                
             }
 
             if (totalCompleted == taskList.Count)
