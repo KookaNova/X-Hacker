5e1127dbfd25403b073fa21510a4ad0885f02b4f
Brandon <brandonjorgen@gmail.com>
2020-10-30 11:36:15 -0600

    Win and Lose menus created

---
+++ X-Hacker Project/Assets/Scripts/ValueCompareSO.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class ValueCompareSO : ScriptableObject
+{
+    public float comparedValue;
+    public FloatDataSO valueData;
+    public bool greaterThan, lessThan, equals;
+    public UnityEvent greaterThanEvent, lessThanEvent, equalsEvent, greaterThanEqualsEvent, lessThanEqualsEvent;
+
+    public void CompareValues()
+    {
+        if (greaterThan && lessThan)
+        {
+            Debug.LogError("You can't check for both greater and less than at the same time!");
+            return;
+        }
+
+        if (greaterThan)
+        {
+            if (valueData.value > comparedValue)
+            {
+                greaterThanEvent.Invoke();
+
+                if (equals)
+                {
+                    greaterThanEqualsEvent.Invoke();
+                }
+            }
+        }
+
+        if (lessThan)
+        {
+            if (valueData.value < comparedValue)
+            {
+                lessThanEvent.Invoke();
+
+                if (equals)
+                {
+                    lessThanEqualsEvent.Invoke();
+                }
+            }
+        }
+
+        if (equals)
+        {
+            if (valueData.value == comparedValue)
+            {
+                equalsEvent.Invoke();
+            }
+        }
+    }
+}
