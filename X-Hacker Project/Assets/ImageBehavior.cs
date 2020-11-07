688d7d9d89e21b1c6207aee8993985c3af445531
Brandon <brandonjorgen@gmail.com>
2020-11-07 13:52:53 -0700

    plug in printer minigame created

---
+++ X-Hacker Project/Assets/ImageBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ImageBehavior : MonoBehaviour
+{
+    private Image image;
+    public UnityEvent startEvent;
+    
+    public void OnEnable()
+    {
+        image = GetComponent<Image>();
+        startEvent.Invoke();
+    }
+
+    public void ChangeColor(Color32 color)
+    {
+        image.color = color;
+    }
+
+    public void ChangeAlpha(float value)
+    {
+        image.color = new Color(image.color.r, image.color.g, image.color.b, value);
+    }
+}
