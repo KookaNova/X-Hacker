0c88aa54496ac431f12397ed458dbae5fed4ed21
Brandon <brandonjorgen@gmail.com>
2020-11-07 16:14:21 -0700

    Computer Mini-Game updated

--- X-Hacker Project/Assets/ImageBehavior.cs
+++ X-Hacker Project/Assets/ImageBehavior.cs
@@ -1,16 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class ImageBehavior : MonoBehaviour
 {
     private Image image;
-    public UnityEvent startEvent;
+    private bool increaseFill;
+    public float fillSpeed = 0;
+    public UnityEvent startEvent, maxFillAmountEvent;
     
     public void OnEnable()
     {
         image = GetComponent<Image>();
         startEvent.Invoke();
+    }
+
+    private void Update()
+    {
+        if (image.fillAmount != 1 && increaseFill)
+        {
+            image.fillAmount += fillSpeed * Time.deltaTime;
+        } 
+        else if (image.fillAmount == 1)
+        {
+            maxFillAmountEvent.Invoke();
+        }
     }
 
     public void ChangeColor(Color32 color)
@@ -21,5 +36,10 @@ public class ImageBehavior : MonoBehaviour
     public void ChangeAlpha(float value)
     {
         image.color = new Color(image.color.r, image.color.g, image.color.b, value);
+    }
+
+    public void StartFillBool(bool value)
+    {
+        increaseFill = value;
     }
 }
