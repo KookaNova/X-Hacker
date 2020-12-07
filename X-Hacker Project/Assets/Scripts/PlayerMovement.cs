dfe39925e6a6860e64d07295214b65e3503d6011
Brandon <brandonjorgen@gmail.com>
2020-12-07 16:59:34 -0700

    Particle Scripting complete

--- X-Hacker Project/Assets/Scripts/PlayerMovement.cs
+++ X-Hacker Project/Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine; 
- 
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
 [RequireComponent(typeof(CharacterController))] 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f, gravity = 1f; 
-    private Vector3 _direction; 
+    public float speed = 5f, gravity = 1f;
+    public UnityEvent movingEvent;
+    private Vector3 _direction, movement; 
     private CharacterController _controller;
     private bool canMove;
  
@@ -21,13 +24,13 @@ public class PlayerMovement : MonoBehaviour
             _direction.x = Input.GetAxis("Horizontal"); 
             _direction.z = Input.GetAxis("Vertical"); 
  
-            var movement = new Vector3(_direction.x, 0, _direction.z); 
+            movement = new Vector3(_direction.x, 0, _direction.z); 
  
             if (movement != Vector3.zero) 
             { 
                 transform.rotation = Quaternion.Slerp(transform.rotation, 
-                    Quaternion.LookRotation(movement), 0.25f); 
-            } 
+                    Quaternion.LookRotation(movement), 0.25f);
+            }
          
             movement.y -= gravity * 9 * Time.deltaTime; 
             _controller.Move(movement * speed * Time.deltaTime);
